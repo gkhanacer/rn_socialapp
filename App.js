77d89b2edd3f26ff94fe4d068d92c838575d8b31
gokhan-rabo <gokhan-rabo@gokhans-MacBook-Pro.local>
2023-08-23 23:51:51 +0200

    Font loader new version code removed. Old version used

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import Providers from "./navigation";
 
-// import { useFonts } from "expo-font";
-// import * as SplashScreen from "expo-splash-screen";
-
 import AppLoading from "expo-app-loading";
 import useCustomFonts from "./hooks/useCustomFonts";
 
@@ -25,21 +22,6 @@ const App = () => {
       />
     );
   }
-
-  // const [fontsLoaded] = useFonts({
-  //   "Kufam-SemiBoldItalic": require("./assets/fonts/Kufam-SemiBoldItalic.ttf"),
-  //   "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
-  // });
-
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
-
-  // if (!fontsLoaded) {
-  //   return null;
-  // }
 
   return (<Providers/>);
 };
